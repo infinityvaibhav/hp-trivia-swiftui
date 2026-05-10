eb7e1934bad72b9e39515727d0f112da9443313e
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2026-05-10 12:45:39 +0530

    Initial Commit

---
+++ HPTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HPTrivia
+//
+//  Created by वैभव उपाध्याय on 10/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by वैभव उपाध्याय on 10/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
