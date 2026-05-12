a2a78c82333f8495c23365333c181c1e71f3202a
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2026-05-12 07:00:36 +0530

    [feat] started game logic - game properties, start game and end game

--- HPTrivia/ViewModel/GameViewModel.swift
+++ HPTrivia/ViewModel/GameViewModel.swift
@@ -9,4 +9,38 @@ import SwiftUI
 @Observable
 class GameViewModel {
     var bookQuestions = BookQuestions()
+    
+    var gameScore = 0
+    var questionScore = 0
+    var recentScores = [0, 0, 0]
+    
+    var activeQuestions: [Question] = []
+    var answeredQuestions: [Int] = []
+    var currentQuestion: Question = try! JSONDecoder().decode([Question].self, from: Data(contentsOf: Bundle.main.url(forResource: "trivia", withExtension: "json")!))[0]
+    var answers: [String] = []
+    
+    func startGame() {
+        activeQuestions = bookQuestions.books
+                            .filter { $0.status == .active }
+                            .flatMap { $0.questions }
+        newQuestion()
+    }
+    
+    func newQuestion() {
+        
+    }
+    
+    func correct() {
+        
+    }
+    
+    func endGame() {
+        recentScores[2] = recentScores[1]
+        recentScores[1] = recentScores[0]
+        recentScores[0] = gameScore
+        
+        gameScore = 0
+        activeQuestions = []
+        answeredQuestions = []
+    }
 }
